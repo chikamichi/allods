5b96039ad6344cc74dd97b1beea8da14e1fae07d
Jean-Denis Vauguet <jd@vauguet.fr>
2011-02-27 22:12:47 +0100

    streamline deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,38 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "bundler/capistrano"
 
-set :use_sudo, false
+# Generic setup.
 set :user, 'jd'
-
+set :use_sudo, false
+set :rvm_type, :user
 set :scm, :git
 set :scm_username, :git
 set :deploy_via, :remote_cache
+set :application, "hugr.fr"
+set :repository,  "git://github.com/chikamichi/allods.git"
+set :branch, 'master'
 
+# Specific deploy environments setups.
 desc "development env"
 task :env_dev do
-  set :application, "allods"
-  set :repository,  "."
-  set :branch, 'master'
-  set :deploy_to, "/home/jd/allods/web"
+  set :rvm_ruby_string, 'ruby-1.9.2@allods-dev'
+  set :deploy_to, "/home/jd/allods/webapp.dev"
   set :application_type, "development"
   set :rails_env, "development"
   set :default_environment, {"RAILS_ENV" => rails_env}
-  server "#{user}@#{application}-#{rails_env}", :app, :web, :db, :primary => true
-
-  set :scm, :subversion
-  # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-  role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-  role :app, "your app-server here"                          # This may be the same as your `Web` server
-  role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-  role :db,  "your slave db-server here"
+  server "#{user}@#{application}", :app, :web, :db, :primary => true
 end
+
+# Tasks to perform.
+namespace :bundle do
+  desc "Install required gems"
+  task :install do
+    run "cd #{current_path} && bundle install --deployment --without production"
+  end
+end
+
+# Let's proceed!
+after "deploy:update_code", "bundle:install"
+after "bundle:install", "db:create"
+after "db:create", "deploy:migrate"
+after "deploy:migrate", "deploy:restart"
