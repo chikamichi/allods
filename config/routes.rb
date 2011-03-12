baa5546aaa2ea74a6a2547b85e394ba03de0e055
Jean-Denis Vauguet <jd@vauguet.fr>
2011-03-12 20:22:30 +0100

    localized routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Allods::Application.routes.draw do
   resources :home,   :only => :index
   resources :admins, :only => :index
 
-  resources :characters
-  resources :loot_machines
+  resources :personnages, :controller => :characters, :as => :characters
+  resources :groupes, :controller => :loot_machines, :as => :loot_machines
 
   root :to => 'home#index'
 end
