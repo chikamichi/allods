ede64ac18d46d125541e41bb74d82b64a6bdb3cc
Jean-Denis Vauguet <jd@vauguet.fr>
2011-03-14 15:10:54 +0100

    deploy.rb update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require 'rvm/capistrano'
-require './capistrano_database_yml'
+require File.expand_path './config/capistrano_database_yml.rb'
 
 set :rvm_ruby_string, 'ruby-1.9.2@allods'
 set :rvm_type, :user
